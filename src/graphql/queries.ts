4d4c8f7a3f819e2a2971de2dfb306299ec9941ee
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-15 13:44:03 -0300

    feat: create function to generateQueryGetCityByName

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -36,3 +36,43 @@ export const GET_CITY_BY_NAME = gql`
     }
   }
 `;
+
+export function generateQueryGetCityByName(city: string) {
+  const query = `
+    {
+        getCityByName(name: ${city}, config: { units: metric }) {
+          name
+          country
+          coord {
+            lon
+            lat
+          }
+          weather {
+            timestamp
+            summary {
+              title
+              description
+              icon
+            }
+            temperature {
+              actual
+              feelsLike
+              min
+              max
+            }
+            wind {
+              speed
+              deg
+            }
+            clouds {
+              all
+              visibility
+              humidity
+            }
+          }
+        }
+      }
+    `;
+
+  return query;
+}
