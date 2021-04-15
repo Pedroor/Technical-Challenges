232cdf728fb8e99058a3592ade83222966cf2305
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-15 14:04:16 -0300

    feat: create function setStore to persist city in AsyncStorage

--- src/hooks/useFavoriteCityList.tsx
+++ src/hooks/useFavoriteCityList.tsx
@@ -6,6 +6,7 @@ import React, {
   useEffect,
 } from "react";
 import AsyncStorage from "@react-native-community/async-storage";
+import { City } from "../graphql/types";
 import { Alert } from "react-native";
 
 type FavoriteCityListProviderProps = {
@@ -21,6 +22,15 @@ export const FavoriteCityContext = createContext<FavoriteCityListContextData>(
 export function FavoriteCityListProvider({
   children,
 }: FavoriteCityListProviderProps) {
+  const [favoriteCityList, setFavoriteCityList] = useState<City[]>([]);
+
+  async function setStore(favoriteCity: City[]) {
+    await AsyncStorage.setItem(
+      "@FavoriteCityList",
+      JSON.stringify(favoriteCity)
+    );
+  }
+
   return (
     <FavoriteCityContext.Provider value={{}}>
       {children}
