8c1fe2e944f80a40492df6c7bce79bc0ecca03a1
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-03-19 12:20:28 -0300

    styles: creating theme and add primary font

---
+++ cinemapp/src/styles/theme.ts
@@ -0,0 +1,43 @@
+export default {
+  colors: {
+    black: '#050505',
+    white: '#f0f2f5',
+    grey: '#919191',
+    lowGrey: '#d2d6d5',
+    lightGrey: '#E7E7E8',
+    semiGrey: '#F4F5F4',
+    red: '#FA6555',
+    green: '#41D168',
+    blue: '#0055D4',
+    lilac: '#6C79DB',
+    title: '#f2231f',
+  },
+
+  textVariants: {
+    title: {
+      fontSize: '26px',
+      fontFamily: 'Montserrat-Bold',
+      lineHeight: '22px',
+    },
+    body1: {
+      fontSize: '18px',
+      fontFamily: 'Montserrat_700Bold',
+      lineHeight: '22px',
+    },
+    body2: {
+      fontSize: '16px',
+      fontFamily: 'Montserrat-SemiBold',
+      lineHeight: '20px',
+    },
+    body3: {
+      fontSize: '14px',
+      fontFamily: 'Montserrat_300Light',
+      lineHeight: '18px',
+    },
+    input: {
+      fontSize: '14px',
+      fontFamily: 'Montserrat_400Regular',
+      lineHeight: '18px',
+    },
+  },
+} as const;
