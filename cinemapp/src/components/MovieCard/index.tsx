d19133306ac87491265ce7a357a4c823224f10c1
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-03-20 19:49:38 -0300

    feat: add movieCard component

--- cinemapp/src/components/MovieCard/index.tsx
+++ cinemapp/src/components/MovieCard/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import * as S from './styles';
+import { MovieCardProps } from './types';
 
-const MovieCard: React.FC = () => {
-  return <View />;
+const MovieCard: React.FC<MovieCardProps> = ({ item }) => {
+  //   const [isColorStar, setIsColorStar] = useState(false);
+
+  console.log('OLA', item);
+  return (
+    <S.Card key={item.imdbID}>
+      <S.TitleContainer>
+        <S.TitleName>
+          {item.Title.length > 60
+            ? `${item.Title.substring(0, 60)}...`
+            : item.Title}
+        </S.TitleName>
+      </S.TitleContainer>
+      <S.ImageCard source={{ uri: `${item.Poster}` }} />
+      <S.CardContent>
+        {/* {favoriteHeroes ? (
+            <Entypo
+              name="star"
+              size={24}
+              color={isColorStar ? "#eaf754" : "black"}
+              style={{ padding: s(8) }}
+              onPress={() => handleAddHeroToFavoriteList(item)}
+            />
+          ) : (
+            <Entypo
+              name="star"
+              size={24}
+              color={"black"}
+              style={{ padding: s(8) }}
+              onPress={() => handleAddHeroToFavoriteList(item)}
+            />
+          )} */}
+      </S.CardContent>
+    </S.Card>
+  );
 };
 
 export default MovieCard;
--- cinemapp/src/components/MovieCard/styles.ts
+++ cinemapp/src/components/MovieCard/styles.ts
@@ -1,3 +1,50 @@
 import styled from 'styled-components/native';
+import { s, ms } from 'react-native-size-matters';
 
 export const Container = styled.View``;
+export const Card = styled.View`
+  width: 90%;
+  height: ${s(240)};
+
+  border-radius: ${s(40)};
+  margin-bottom: ${ms(30)};
+  margin-left: ${ms(10)};
+`;
+
+export const CardContent = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+export const TitleName = styled.Text`
+  ${({ theme }) => theme.textVariants.body1};
+  text-align: center;
+  padding: 20px 10px 10px 10px;
+  font-weight: bold;
+`;
+
+export const Label = styled.Text`
+  ${({ theme }) => theme.textVariants.body2};
+  padding-top: ${s(60)};
+  padding-left: ${s(10)};
+  font-weight: bold;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ImageCard = styled.Image`
+  width: ${s(180)};
+  height: ${s(220)};
+  border-radius: 15px;
+`;
+
+export const DetailsButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  position: relative;
+  bottom: ${s(0)};
+  top: ${s(-15)};
+`;
