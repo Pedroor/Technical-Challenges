d2abe7cbaa3fedc76003788f57fea6ce61f1dc79
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-03-20 09:54:49 -0300

    config: add structure of screens and components

---
+++ cinemapp/src/screens/FavoriteList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const FavoriteList: React.FC = () => {
+  return <View />;
+};
+
+export default FavoriteList;
