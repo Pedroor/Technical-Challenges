85f215965a9253c70e7b9d8925deb7200defebe7
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-06-12 10:55:06 -0300

    fix: removing pods file and add to .gitignore

--- ios/Pods/Headers/Private/ReactNativeGetLocation/SettingsUtil.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-get-location/ios/ReactNativeGetLocationLibrary/SettingsUtil.h
--- ios/Pods/Target Support Files/Pods-weather-weatherTests/Pods-weather-weatherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_weather_weatherTests : NSObject
-@end
-@implementation PodsDummy_Pods_weather_weatherTests
-@end
